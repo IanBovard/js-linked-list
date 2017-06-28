e394f6ed349c99061c762b2764915b929ede453e
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-28 11:04:23 -1000

    add

--- linkedList.js
+++ linkedList.js
@@ -5,15 +5,15 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
  function linkedListGenerator(){
-  let list = null;
+  let linkedList = null;
 
   function getHead(){
-    return list;
+    return linkedList;
   }
   function getTail(){
-    let currentNode = list;
-    if (currentNode){
-      while(currentNode.next){
+    let currentNode = linkedList;
+    if (currentNode !== null){
+      while (currentNode.next){
         currentNode = currentNode.next;
       }
       return currentNode;
@@ -22,63 +22,67 @@
     }
   }
   function add(value){
-    let newNode = {
+    let tail = getTail();
+    let currentNode = linkedList;
+    if (tail === null){
+      linkedList  = {
+        value : value,
+        next : null
+      };
+      return linkedList;
+    }else{
+     tail.next = {
       value : value,
       next : null
     };
-    let currentNode = list;
-    let tail = getTail();
-    if (tail === null){
-      list = newNode;
-      return newNode;
-    }else{
-      currentNode.next = newNode;
-      return currentNode.next;
-    }
-    return null;
   }
-  function get(number){
-    let currentNode = list;
-    if (number === 0){
-      return currentNode;
-    }else if (number < 0){
-      return false;
-    }else if (typeof number === "string"){
-      return false;
-    }
-    for (let i = 1; i <= number; i++){
-      if (currentNode){
-        currentNode = currentNode.next;
-      }else{
-        return false;
-      }
-    }
-    return currentNode;
-  }
-  function remove(number){
-
-  }
-  function insert(value, number){
-
-  }
-  return {
-    getHead : getHead,
-    getTail : getTail,
-    add : add,
-    get : get,
-    remove : remove,
-    insert : insert
-  };
+  return tail.next;
 }
-let li = linkedListGenerator();
-console.log("head",li.getHead());
-console.log("tail",li.getTail());
-console.log(li.add("dog"));
-console.log(li.add("cat"));
-console.log("one",li.get(1));
-console.log("zero",li.get(0));
-console.log("head",li.getHead());
-console.log("tail",li.getTail());
-console.log(li.insert("bird", 1));
-console.log(li.remove(2));
-console.log("tail",li.getTail());
+
+function get(number){
+ let currentNode = linkedList;
+ let count  = 0;
+ if (count === number){
+  return currentNode;
+}else if (number < 0){
+  return false;
+}else{
+  while (count < number){
+    if (currentNode.next !== null){
+      currentNode = currentNode.next;
+      count++;
+    }else{
+  return false;
+}
+  }
+      return currentNode;
+}
+}
+function remove(number){
+
+}
+function insert(value, number){
+
+}
+return {
+  getHead : getHead,
+  getTail : getTail,
+  add : add,
+  get : get,
+  remove : remove,
+  insert : insert
+};
+}
+/*let test = linkedListGenerator();
+console.log("head:",test.getHead());
+console.log("tail: ",test.getTail());
+console.log("add: ",test.add('cat'));
+console.log("head: ",test.getHead());
+console.log("tail: ",test.getTail());
+console.log("add: ",test.add('dog'));
+console.log("head: ",test.getHead());
+console.log("tail: ",test.getTail());
+console.log("add: ",test.add('fish'));
+console.log("head: ",test.getHead());
+console.log("tail: ",test.getTail());
+console.log("get: ", test.get());*/
